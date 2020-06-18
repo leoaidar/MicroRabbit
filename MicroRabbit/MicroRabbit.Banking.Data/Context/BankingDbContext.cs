00bea3e3e71b442e635fd69e1d22500dad213e24
Léo Aidar <leofla@gmail.com>
2020-06-18 00:31:35 -0300

    MIgration and Controler and Services in Startup

--- MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -8,9 +8,19 @@ namespace MicroRabbit.Banking.Data.Context
 {
     public class BankingDbContext : DbContext
     {
-        public BankingDbContext(DbContextOptions options) : base(options)
+        public BankingDbContext() { }
+
+        public BankingDbContext(DbContextOptions<BankingDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=BankingDB;User ID=SA;Password=Mudar@4321;MultipleActiveResultSets=true;");
+            }
         }
 
         public DbSet<Account> Accounts { get; set; }
