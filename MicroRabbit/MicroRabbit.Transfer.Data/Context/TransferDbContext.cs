e8548e47666b725a97076e5c37d72c1107b10067
Léo Aidar <leofla@gmail.com>
2020-06-19 02:50:02 -0300

    Implementing Interfaces and Classes of Transfer Microservice

--- MicroRabbit/MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
+++ MicroRabbit/MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MicroRabbit.Banking.Data.Context
+namespace MicroRabbit.Transfer.Data.Context
 {
     public class TransferDbContext : DbContext
     {
